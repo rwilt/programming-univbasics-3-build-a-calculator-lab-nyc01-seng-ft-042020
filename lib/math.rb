b4061d6ed16d90e061f905cab1eb856aa60c31ae
rosie w <rosie.wilt@gmail.com>
2020-04-06 16:19:18 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -4,21 +4,26 @@ sum
 end
 
 def subtraction(num1, num2)
-
+sum = num1 - num2
+sum
 end
 
 def division(num1, num2)
-
+sum = num1 / num2
+sum
 end
 
 def multiplication(num1, num2)
-
+sum = num1 * num2
+sum
 end
 
 def modulo(num1, num2)
-
+sum = num1 % num2
+sum
 end
 
 def square_root(num)
-
+  sum = num1 ^ num2
+  sum
 end
